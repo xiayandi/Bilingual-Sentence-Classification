0e6fc1d78aa2380f1585382b25df457cdbaf81dd
xiayandi <xiayandi@gmail.com>
2015-09-11 16:17:32 -0500

    building day#3, adding Chinese preprocessing and mix bilingual corpus building

--- code/buildBilingualCorpus.py
+++ code/buildBilingualCorpus.py
@@ -1,9 +1,43 @@
 __author__ = 'yandixia'
 
 import re
+import codecs
+import numpy as np
 
 
-def word_filter(word):
+chinese_punctuation = u'\uFF01\uFF1F\uFF61\u3002\uFF02\uFF03\uFF04\uFF05\uFF06\uFF07\uFF08\uFF09\uFF0A\uFF0B\uFF0C\uFF0D\uFF0F\uFF1A\uFF1B\uFF1C\uFF1D\uFF1E\uFF20\uFF3B\uFF3C\uFF3D\uFF3E\uFF3F\uFF40\uFF5B\uFF5C\uFF5D\uFF5E\uFF5F\uFF60\uFF62\uFF63\uFF64\u3000\u3001\u3003\u3008\u3009\u300A\u300B\u300C\u300D\u300E\u300F\u3010\u3011\u3014\u3015\u3016\u3017\u3018\u3019\u301A\u301B\u301C\u301D\u301E\u301F\u3030\u303E\u303F\u2013\u2014\u2018\u2019\u201B\u201C\u201D\u201E\u201F\u2026\u2027\uFE4F\uFE51\uFE54\u00B7'
+
+
+def chinese_word_filter(word):
+    """
+    func: preprocess word level
+    params: word: the word to be preprocessed
+    return: a list of seperated words. [] if word is to be removed.
+    """
+    # punctuations like [,.-'"/;]
+    if word in chinese_punctuation:
+        return []
+
+    # filter for url
+    isURL = word[:3] == 'www' or '.com' in word or '.org' in word \
+            or '.uk' in word
+    if isURL:
+        return ['URL']
+
+    # filter for year
+    pattern = '^[12][0-9]{3}$'
+    if re.match(pattern, word):
+        return ['YEAR']
+
+    # filter for numbers
+    pattern = "^[0-9\,\.\-]+$"
+    if re.match(pattern, word):
+        return ['NUMBER']
+
+    return [word]
+
+
+def english_word_filter(word):
     """
     func: preprocess word level
     params: word: the word to be preprocessed
@@ -15,7 +49,7 @@ def word_filter(word):
         return [word]
 
     # punctuations like [,.-'"/;]
-    if word in [',', '.', '-', '--', '\'', '\"', ';', '/']:
+    if word in "\"'?/[.,-\/#!$%\^&\*;:{}=\-_`~()]/":
         return []
 
     # filter for url
@@ -61,12 +95,44 @@ def preprocessEnglishCorpus(CorpusFile, preprocessedFile):
             words = line.split()
             newwords = []
             for word in words:
-                newwords.extend(word_filter(word))
+                newwords.extend(english_word_filter(word))
             outputbuffer.append(' '.join(newwords) + '\n')
         print 'writing buffer...'
         with open(preprocessedFile, 'a') as writer:
             writer.writelines(outputbuffer)
     reader.close()
 
+
+def preprocessChineseCorpus(CorpusFile, preprocessedFile):
+    """
+    func: preprocess corpus
+    params: CorpusFile: corpus file path
+    params: preprocessedFile: the output preprocessed file
+    return: n/a
+    """
+    print 'preprocessing...'
+    print 'reading corpus...'
+    with codecs.open(CorpusFile, 'r', 'utf-8') as reader:
+        corpuslines = reader.readlines()
+    print 'reading corpus done...'
+
+    buffsize = 1000000
+    buffnum = len(corpuslines) / buffsize + 1
+    codecs.open(preprocessedFile, 'w', 'utf-8').close()
+    for i in xrange(buffnum):
+        print 'building with ' + str(i) + ' buffer.....'
+        outputbuffer = []
+        bufflines = corpuslines[i * buffsize:(i + 1) * buffsize]
+        for line in bufflines:
+            words = line.split()
+            newwords = []
+            for word in words:
+                newwords.extend(chinese_word_filter(word))
+            outputbuffer.append(' '.join(newwords) + '\n')
+        print 'writing buffer...'
+        with codecs.open(preprocessedFile, 'a', 'utf-8') as writer:
+            writer.writelines(outputbuffer)
+    reader.close()
+
 if __name__ == '__main__':
-    preprocessEnglishCorpus('../data/1bwlmb', '../data/pre_1bwlmb')
+    preprocessChineseCorpus('../data/gigacorpus', '../data/pre_gigacorpus')
--- code/test.py
+++ code/test.py
@@ -2,26 +2,36 @@
 this module is for test only
 """
 
-vocablst = set()
-with open('../data/eng_dict_desc.lst', 'r') as reader:
-    datalines = reader.readlines()
-for line in datalines:
-    if line.strip():
-        items = line.split()
-        if len(items)<2:
-            continue
-        if items[0] == 'Usage':
-            continue
-        if items[0][0]=='-' or items[0][-1]=='-':
-            continue
-        if items[0][0]=='\'':
-            continue
-        if items[0][-1] in [str(i) for i in xrange(10)]:
-            vocablst.add(items[0][:-1].lower())
-        else:
-            vocablst.add(items[0].lower())
-vocablst.add('usage')
-vocablst = sorted(list(vocablst))
-with open('../exp/dict.lst', 'w') as writer:
-    for word in vocablst:
-        writer.write(word+'\n')
+
+def getVocabFromEnglishDictionary():
+    vocablst = set()
+    with open('../data/eng_dict_desc.lst', 'r') as reader:
+        datalines = reader.readlines()
+    for line in datalines:
+        if line.strip():
+            items = line.split()
+            if len(items) < 2:
+                continue
+            if items[0] == 'Usage':
+                continue
+            if items[0][0] == '-' or items[0][-1] == '-':
+                continue
+            if items[0][0] == '\'':
+                continue
+            if items[0][-1] in [str(i) for i in xrange(10)]:
+                vocablst.add(items[0][:-1].lower())
+            else:
+                vocablst.add(items[0].lower())
+    vocablst.add('usage')
+    vocablst = sorted(list(vocablst))
+    with open('../exp/dict.lst', 'w') as writer:
+        for word in vocablst:
+            writer.write(word + '\n')
+
+
+def testUnicode():
+    print u'》'
+
+
+if __name__ == '__main__':
+    testUnicode()
