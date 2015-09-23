8db40d1bd4c2be7b5e43f5ac252a6fa166114adc
xiayandi <xiayandi@gmail.com>
2015-09-23 14:20:10 -0500

    building day#8, adding ancestor pattern operation

--- code/test.py
+++ code/test.py
@@ -82,7 +82,7 @@ def getAncestors(idx, level, d2g):
     """
     if level == 1:
         if idx in d2g:
-            return d2g[idx][0]
+            return [d2g[idx][0]]
         else:
             return None
 
@@ -91,19 +91,44 @@ def getAncestors(idx, level, d2g):
         for g in gs:
             ga = getAncestors(g, level-1, d2g)
             if ga is None:
-                return None
+                continue
             else:
-                return g+ga
-    else:
-        return None
+                return [g] + ga
+    return None
 
 
+def getSiblings(idx, sibnum, g2d, d2g):
+    """
+    :func get the siblings give the number constraint
+    :param idx: node index
+    :param sibnum: sibling number constraint
+    :param g2d: governor to dependent mapping
+    :param d2g: dependent to governor mapping
+    :return: return None if sibnum not fulfilled,
+            return a list of siblings otherwise
+    """
+    gs = d2g[idx]
+    for g in gs:
+        if len(g2d[g]) == sibnum:
+            return g2d[g]
+    return None
+
+
+def getSiblingPatterns(idx, sibnum, ancestornum, g2d, d2g):
+    sibs = getSiblings(idx, sibnum, g2d, d2g)
+    if sibs:
+        ancs = getAncestors(idx, ancestornum, d2g)
+        if ancs:
+            return sibs + ancs
+    return None
 
 
 def testRundown():
-    sentences_triples = readInDependencyTriples('../exp/deps')
-    for sent_triple in sentences_triples:
-        getTreeStructure(sent_triple)
+    triple = [(0, 'root', 4), (3, 'assmod', 1), (1, 'case', 2), (4, 'dep', 3), (6, 'xsubj', 3), (6, 'advmod', 5),
+              (7, '', 6), (8, '', 7)]
+    g2d, d2g = getTreeStructure(triple)
+    sibs = getSiblings(3, 2, g2d, d2g)
+    print sibs
 
 
 if __name__ == '__main__':
