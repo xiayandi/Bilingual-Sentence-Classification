5eeabb95ba0380382f5b911ec97469c8938b16b6
xiayandi <xiayandi@gmail.com>
2015-12-19 12:15:31 -0600

    final exp

--- code/batchofexp.py
+++ code/batchofexp.py
@@ -66,49 +66,16 @@ def script():
 
     preprocess_qc.splitValidOut(testbase, 314)
 
-    # PR dep, phr, lex
-    cdep = False
-    phr = True
-    lex = True
-    logprefix = '../exp/dep_phr_lex'
-    filter_hss = [[1,3]]
-    config_2 = config(trainbase, 'eng', testbase, validbase, lex, phr, cdep, logprefix)
-    word2vec.rundown_config(config_2)
-    process_qc.datasetConstructRundown_config(config_2)
-    cnn_model.script(config_2, filter_hss)
-
-    # PR dep, lex
-    cdep = False
-    phr = False
-    lex = True
-    logprefix = '../exp/dep_lex'
-    filter_hss = [[1,3]]
-    config_3 = config(trainbase, 'eng', testbase, validbase, lex, phr, cdep, logprefix)
-    word2vec.rundown_config(config_3)
-    process_qc.datasetConstructRundown_config(config_3)
-    cnn_model.script(config_3, filter_hss)
-
-    # PR dep
+    # PR translated
     cdep = False
     phr = False
     lex = False
-    logprefix = '../exp/dep'
-    filter_hss = [[3]]
-    config_4 = config(trainbase, 'eng', testbase, validbase, lex, phr, cdep, logprefix)
-    word2vec.rundown_config(config_4)
-    process_qc.datasetConstructRundown_config(config_4)
-    cnn_model.script(config_4, filter_hss)
-
-    # PR translated
-#   cdep = False
-#   phr = False
-#   lex = False
-#   logprefix = 'translated'
-#   filter_hss = [[3],[3,4],[3,4,5]]
-#   config_5 = config(translate_base, 'ch', testbase, validbase, lex, phr, cdep, logprefix)
-#   word2vec.rundown_config(config_5)
-#   process_qc.datasetConstructRundown_config(config_5)
-#   cnn_model.script(config_5, filter_hss)
+    logprefix = 'translated'
+    filter_hss = [[3],[3,4],[3,4,5]]
+    config_5 = config(translate_base, 'ch', testbase, validbase, lex, phr, cdep, logprefix)
+    word2vec.rundown_config(config_5)
+    process_qc.datasetConstructRundown_config(config_5)
+    cnn_model.script(config_5, filter_hss)
 
 
 if __name__ == '__main__':
