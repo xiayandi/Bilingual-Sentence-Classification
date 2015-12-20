f3635ac43f30dc5c98ba4758d211061b107dd4de
xiayandi <xiayandi@gmail.com>
2015-12-19 22:54:28 -0600

    final exp

--- code/batchofexp.py
+++ code/batchofexp.py
@@ -6,6 +6,7 @@ import process_qc
 import cnn_model
 import preprocess_qc
 
+
 class config:
     def __init__(self, trainbase, trainlang, testbase, validbase, lexicon, phrase, cdep, logprefix):
         self.lexicon = lexicon
@@ -18,69 +19,87 @@ class config:
             if phrase:
                 self.allw2v = '../data/blg250_all_phrase.txt'
                 self.trainbase = trainbase
-                self.trainfile = trainbase+'.phr'
+                self.trainfile = trainbase + '.phr'
                 if cdep:
-                    self.traindep = self.trainfile+'.cdep'
+                    self.traindep = self.trainfile + '.cdep'
                 else:
-                    self.traindep = self.trainfile+'.dep'
+                    self.traindep = self.trainfile + '.dep'
             else:
                 self.allw2v = '../data/blg250.txt'
                 self.trainbase = trainbase
                 self.trainfile = trainbase
                 if cdep:
-                    self.traindep = self.trainfile+'.cdep'
+                    self.traindep = self.trainfile + '.cdep'
                 else:
-                    self.traindep = self.trainfile+'.dep'
+                    self.traindep = self.trainfile + '.dep'
         elif trainlang == 'ch':
-                self.allw2v = '../data/ch_250.txt'
-                self.trainbase = trainbase
-                self.trainfile = trainbase+'.seg'
-                if cdep:
-                    self.traindep = self.trainbase+'.cdep'
-                else:
-                    self.traindep = self.trainbase+'.dep'
+            self.allw2v = '../data/ch_250.txt'
+            self.trainbase = trainbase
+            self.trainfile = trainbase + '.seg'
+            if cdep:
+                self.traindep = self.trainbase + '.cdep'
+            else:
+                self.traindep = self.trainbase + '.dep'
         else:
             print 'wrong language!'
             sys.exit()
 
         self.testbase = testbase
-        self.testfile = testbase+'.seg'
+        self.testfile = testbase + '.seg'
         if cdep:
-            self.testdep = testbase+'.cdep'
+            self.testdep = testbase + '.cdep'
         else:
-            self.testdep = testbase+'.dep'
+            self.testdep = testbase + '.dep'
 
         self.validbase = validbase
-        self.validfile = validbase+'.seg'
+        self.validfile = validbase + '.seg'
         if cdep:
-            self.validdep = validbase+'.cdep'
+            self.validdep = validbase + '.cdep'
         else:
-            self.validdep = validbase+'.dep'
+            self.validdep = validbase + '.dep'
 
 
 def script():
-    trainbase = '../data/Semantic/productReview/train.dat'
-    translate_base = '../data/Semantic/productReview/moses_train.dat'
-    testbase = '../data/Semantic/productReview/test.dat.new'
-    validbase = '../data/Semantic/productReview/validset'
+    trainbase = '../data/Event/English/sub_train.dat'
+    translate_base = '../data/Event/trainslate/moses_train.dat'
+    chtrainbase = '../data/Event/Chinese/rest_test.dat'
+    testbase = '../data/Event/Chinese/sub_test.dat'
+    validbase = '../data/Event/Chinese/validset'
 
-    preprocess_qc.splitValidOut(testbase, 314)
+    preprocess_qc.splitValidOut(chtrainbase, 500)
 
-    # PR translated
+    # Event: phr, dep, lex
+    cdep = False
+    phr = True
+    lex = True
+    logprefix = 'event_dep_lex_phr'
+    filter_hss = [[1, 3], [1, 3, 4], [1, 3, 4, 5]]
+    config_ = config(trainbase, 'eng', testbase, validbase, lex, phr, cdep, logprefix)
+    word2vec.rundown_config(config_)
+    process_qc.datasetConstructRundown_config(config_)
+    cnn_model.script(config_, filter_hss)
+
+    # Event: dep, lex
+    cdep = False
+    phr = False
+    lex = True
+    logprefix = 'event_dep_lex'
+    filter_hss = [[1, 3], [1, 3, 4], [1, 3, 4, 5]]
+    config_ = config(trainbase, 'eng', testbase, validbase, lex, phr, cdep, logprefix)
+    word2vec.rundown_config(config_)
+    process_qc.datasetConstructRundown_config(config_)
+    cnn_model.script(config_, filter_hss)
+
+    # Event: dep, lex
     cdep = False
     phr = False
     lex = False
-    logprefix = 'translated'
-    filter_hss = [[3],[3,4],[3,4,5]]
-    config_5 = config(translate_base, 'ch', testbase, validbase, lex, phr, cdep, logprefix)
-    word2vec.rundown_config(config_5)
-    process_qc.datasetConstructRundown_config(config_5)
-    cnn_model.script(config_5, filter_hss)
-
+    logprefix = 'event_dep_lex'
+    filter_hss = [[3], [3, 4], [3, 4, 5]]
+    config_ = config(trainbase, 'eng', testbase, validbase, lex, phr, cdep, logprefix)
+    word2vec.rundown_config(config_)
+    process_qc.datasetConstructRundown_config(config_)
+    cnn_model.script(config_, filter_hss)
 
 if __name__ == '__main__':
     script()
-
-
-
-
