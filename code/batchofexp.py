0cf9bc5fa557e3185a7de031c8b45634543a1dc1
xiayandi <xiayandi@gmail.com>
2015-12-20 10:26:08 -0600

    final exp

--- code/batchofexp.py
+++ code/batchofexp.py
@@ -66,40 +66,17 @@ def script():
     testbase = '../data/Event/Chinese/sub_test.dat'
     validbase = '../data/Event/Chinese/validset'
 
-    preprocess_qc.splitValidOut(chtrainbase, 500)
-
-    # Event: phr, dep, lex
+    # Event: dep, lex
     cdep = False
-    phr = True
+    phr = False
     lex = True
-    logprefix = 'event_dep_lex_phr'
+    logprefix = '../exp/event_dep_lex_lex'
     filter_hss = [[1, 3], [1, 3, 4], [1, 3, 4, 5]]
     config_ = config(trainbase, 'eng', testbase, validbase, lex, phr, cdep, logprefix)
     word2vec.rundown_config(config_)
     process_qc.datasetConstructRundown_config(config_)
     cnn_model.script(config_, filter_hss)
 
-    # Event: dep, lex
-    cdep = False
-    phr = False
-    lex = True
-    logprefix = 'event_dep_lex'
-    filter_hss = [[1, 3], [1, 3, 4], [1, 3, 4, 5]]
-    config_ = config(trainbase, 'eng', testbase, validbase, lex, phr, cdep, logprefix)
-    word2vec.rundown_config(config_)
-    process_qc.datasetConstructRundown_config(config_)
-    cnn_model.script(config_, filter_hss)
-
-    # Event: dep, lex
-    cdep = False
-    phr = False
-    lex = False
-    logprefix = 'event_dep_lex'
-    filter_hss = [[3], [3, 4], [3, 4, 5]]
-    config_ = config(trainbase, 'eng', testbase, validbase, lex, phr, cdep, logprefix)
-    word2vec.rundown_config(config_)
-    process_qc.datasetConstructRundown_config(config_)
-    cnn_model.script(config_, filter_hss)
 
 if __name__ == '__main__':
     script()
